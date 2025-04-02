86c23243240f5a83347c0ed81fdfad0ac67c401f
Markus Natanael Sinaga <twinx2blazer@gmail.com>
2025-04-02 18:09:54 +0700

    Update teatransfer.js

--- teatransfer.js
+++ teatransfer.js
@@ -4,8 +4,8 @@ const readline = require("readline");
 // Konfigurasi jaringan Tea Sepolia
 const RPC_URL = "https://tea-sepolia.g.alchemy.com/public";
 const CHAIN_ID = 10218;
-const PRIVATE_KEY = "YOUR_PRIVATE_KEY_HERE";
-const TOKEN_ADDRESS = "YOUR_TOKEN_CONTRACT_ADDRESS";
+const PRIVATE_KEY = "PRIVATE_KEY_KALIAN";
+const TOKEN_ADDRESS = "CONTRACT_ADDRESS";
 
 // ABI ERC-20 minimal untuk transfer token
 const ERC20_ABI = [
@@ -36,7 +36,7 @@ async function main() {
             output: process.stdout
         });
 
-        rl.question("Masukkan jumlah alamat penerima: ", (recipientCountInput) => {
+        rl.question("Masukkan berapa banyak penerima tokennya: ", (recipientCountInput) => {
             const recipientCount = parseInt(recipientCountInput);
             if (isNaN(recipientCount) || recipientCount <= 0) {
                 console.log("Jumlah alamat harus berupa angka positif.");
@@ -47,15 +47,15 @@ async function main() {
             rl.question("Masukkan jumlah token per alamat: ", async (inputAmount) => {
                 const amountToSend = ethers.parseUnits(inputAmount, decimals);
                 const recipients = generateRandomAddresses(recipientCount);
-                console.log(`\n=== Mengirim token ke ${recipientCount} alamat ===\n`);
+                console.log(`\n=== Kirim Token ke ${recipientCount} alamat ===\n`);
 
                 for (let i = 0; i < recipients.length; i++) {
                     try {
                         const tx = await tokenContract.transfer(recipients[i], amountToSend);
                         await tx.wait();
-                        console.log(`${i + 1}. Transaction successfully (${recipients[i]})`);
+                        console.log(`${i + 1}. Transaksi Berhasil (${recipients[i]})`);
                     } catch (error) {
-                        console.log(`${i + 1}. Transaction failed (${recipients[i]}) - ${error.message}`);
+                        console.log(`${i + 1}. Transaksi Gagal (${recipients[i]}) - ${error.message}`);
                     }
                 }
 
